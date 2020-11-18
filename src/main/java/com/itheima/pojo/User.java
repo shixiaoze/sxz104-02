0e09ce7b2be5a8b713dc1ac78271d4f6b9d10cc7
shixiaoze <1163608554@qq.com>
2020-11-18 21:14:06 +0800

    会好

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,13 @@
+package com.itheima.pojo;
+
+import java.util.StringTokenizer;
+
+/**
+ * 包名： com.itheima.pojo
+ *
+ * @author: shixiaoze
+ * 日期: 2020/11/18 21:09
+ */
+public class User {
+    private String name;
+}
